89af62429ffd2d064be5630420edafec3a3f12a3
PixelToast <andre.lipke@gmail.com>
2014-04-20 13:02:19 -0400

    Potato

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,13 +1,16 @@
 tcpnet=socket.connect("71.238.153.166",25476)
 if tcpnet then
+	local file=io.open("tcpnetpassword.txt","r")
+	local port=file:read("*a"):gsub("[\r\n]","")
+	file:close()
 	hook.newsocket(tcpnet)
-	tcpnet:send('{"open",ports={IRC=true}}\n')
+	tcpnet:send('{"open",ports={["'..port..'"]=true}}\n')
 	local cchan="#ocbots"
 	hook.new("select",function()
 		local s=tcpnet:receive()
 		if s then
 			local dat=unserialize(s)
-			if dat.port=="IRC" then
+			if dat.port==port then
 				if dat.data:match("^%.join ") then
 					cchan=dat.data:match("^%S+ (.+)")
 				else
@@ -21,7 +24,7 @@ if tcpnet then
 		if chan==cchan then
 			tcpnet:send(serialize({
 				"send",
-				port="IRC",
+				port=port,
 				data="\194\1677"..(act and ("* "..user.nick.." ") or ("<"..user.nick.."> ")).."\194\167f"..txt
 			}).."\n")
 		end
